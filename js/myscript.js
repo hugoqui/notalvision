b3788483edcbed2f2cdc7f35606cdf2705add766
Hugo Quinonez <hugo_2555@hotmail.com>
2018-12-07 03:24:45 -0600

    active pie element

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,9 @@
-function showDetails(evt, program) {    
+function showDetails(evt, program) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {        
+    for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
-    }    
+    }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
@@ -13,10 +13,20 @@ function showDetails(evt, program) {
 }
 
 function showProgram(program) {
-    tabcontent = document.getElementsByClassName("paragraph");
-    for (i = 0; i < tabcontent.length; i++) {        
+    const tabcontent = document.getElementsByClassName("paragraph");
+    for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
     document.getElementById(program).style.display = "block";
+
+    for (let i = 1; i <= 7; i++) {
+        const element = "active" + i;
+        document.getElementById(element).style.display = "none";
+        const program_name = "paragraph" + i;        
+        const display_program = document.getElementById(program_name).style.display ;    
+        document.getElementById(element).style.display = display_program;
+    }
+    
+
 
 }
